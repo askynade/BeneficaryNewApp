e5339f9be58e1060fbfc5243778a0f685d7b68dc
Akassh Nade <asky.nade@gmail.com>
2022-08-03 19:44:14 +0530

    new requirement .txt

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'i9i&_kg@x0(qdo_h3sqo$7*ihi!1%-5c1v3p@1@^io@ws7bfu2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
